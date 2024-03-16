83b1d7dc9a1bce75da2bd117df23aaa11efd80b9
박태현 <89737399+ttt-hyun@users.noreply.github.com>
2024-03-16 16:26:52 +0900

    test

---
+++ src/app/post/[slug]/page.js
@@ -0,0 +1,53 @@
+import { allDocs } from "contentlayer/generated"
+import { notFound } from "next/navigation";
+import { Mdx } from "@/components/mdx/Mdx";
+// import "highlight.js/styles/night-owl.css";
+import { format } from "date-fns";
+import Image from "next/image";
+import Headings from "@/components/mdx/Headings";
+import { allDocs } from '@/../.contentlayer/generated';
+
+// export const dynamic = 'force-dynamic';
+
+// async function getDocFromParams(slug){
+//     const doc = allDocs.find((doc) => doc.slugAsParams === slug);
+
+//     if(!doc) notFound()
+
+//     return doc
+// }
+
+export const generateStaticParams = async () =>
+    allDocs.map((post) => ({ slug: post._raw.flattenedPath }));
+
+const singlePostPage = async ({ params: { slug } }) => {
+    const doc = allDocs.find((post) => post._raw.flattenedPath === slug);
+
+    if (!doc) notFound();
+
+    return (
+        <>
+        <div className="page-post">
+            <div className="layout-text flex-box justify-content-end align-items-center">
+                <p className="date-text fs12 fw800">{date}</p>
+            </div>
+            <div className="post-thumb rd10">
+                <Image src="/thumb_next_1.svg" alt="" width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto' }} />
+            </div>
+            <div className="post-contents">
+                <div className="title">
+                    <h3>{doc.title}</h3>
+                </div>
+                <div className="contents markdown-body">
+                    <Mdx code={doc.body.code} />
+                </div>
+            </div>
+        </div>
+        <div className="post-heading">
+            <Headings code={doc.body.code} />
+        </div>
+        </>
+    )
+}
+
+export default singlePostPage
